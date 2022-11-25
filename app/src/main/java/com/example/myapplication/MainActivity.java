2d3ceb06cde116528a4938b1caeb08ed2dbb333c
BIL-LAB2PC00\Ogrenci <seyydinburhanedin.mtal38@gmail.com>
2022-11-25 12:14:59 +0300

    no message

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -6,11 +6,15 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
     Button btn6;
     Button btn7;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,7 +25,7 @@ public class MainActivity extends AppCompatActivity {
         Button uygulama4 = findViewById(R.id.btn4);
         btn6 = findViewById(R.id.btn6);
         btn7 = findViewById(R.id.btn7);
-
+        btn7 = findViewById(R.id.button);
 
 
         temelkomutlaragit.setOnClickListener(new View.OnClickListener() {
--- app/src/main/java/com/example/myapplication/Uyg7Activity.java
+++ app/src/main/java/com/example/myapplication/Uyg7Activity.java
@@ -1,30 +1,51 @@
 package com.example.myapplication;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Uyg7Activity extends AppCompatActivity {
+    EditText edittext1,edittext2;
+    
+
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        int x = 10;
-        int y = 3;
-        int toplam = x + y;
-        int fark = x - y;
-        int carpim = x * y;
-        int bolme = x / y;
-        int mod = x % y;
-        x++;
-        y--;
-        System.out.println("Toplam: " + toplam);
-        System.out.println("Fark: " + fark);
-        System.out.println("Çarpım: " + carpim);
-        System.out.println("Bölme: " + bolme);
-        System.out.println("Mod Alma: " + mod);
-        System.out.println("Artırma: " + x);
-        System.out.println("Azaltma: " + y);
+        setContentView(R.layout.uyg7_activity);
+        edittext1= findViewById(R.id.editTextTextMultiLine);
+        edittext2= findViewById(R.id.editTextTextMultiLine2);
+        Button a = findViewById(R.id.button);
+        a.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String a = edittext1.getText().toString();
+                String b = edittext1.getText().toString();
+
+                int x = Integer.parseInt(a);
+                int y = Integer.parseInt(b);
+
+                int toplam = x + y;
+                int fark = x - y;
+                int carpim = x * y;
+                int bolme = x / y;
+                int mod = x % y;
+                x++;
+                y--;
+                System.out.println("Toplam: " + toplam);
+                System.out.println("Fark: " + fark);
+                System.out.println("Çarpım: " + carpim);
+                System.out.println("Bölme: " + bolme);
+                System.out.println("Mod Alma: " + mod);
+                System.out.println("Artırma: " + x);
+                System.out.println("Azaltma: " + y);
+            }
+        });
 
 
     }
